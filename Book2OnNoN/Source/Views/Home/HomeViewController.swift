925dcce2e9f18ca64d45a58e19e8d684e7b9271c
YeoSeongil <seongil_yeo@naver.com>
2024-03-11 23:25:25 +0900

    [STYLE] STYLE HomeViewController

--- Book2OnNoN/Source/Views/Home/HomeViewController.swift
+++ Book2OnNoN/Source/Views/Home/HomeViewController.swift
@@ -6,24 +6,19 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class HomeViewController: BaseViewController {
+    
+    
+    override func setViewController() {
+        super.setViewController()
+        view.backgroundColor = .white
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func setConstraints() {
     }
-    */
+    
 
 }
