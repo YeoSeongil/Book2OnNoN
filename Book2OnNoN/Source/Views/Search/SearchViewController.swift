fee94b53ee9710c5c0a2549c503762bed29c5d8f
YeoSeongil <seongil_yeo@naver.com>
2024-03-11 23:28:04 +0900

    [CHORE] CHORE SearchViewController ✅ UI 컴포넌트 구현 ✅ DropDown 구현 ✅ Bind 구현 : 드롭다운 버튼 클릭 시 item 항목 보이도록 구현, 드롭다운 item 선택 시 선택 한 아이템 viewModel로 binding하여 textField의 Text 값과, 선택 한 아이템을 combine하여 print하는 기능 테스트 (선택 한 아이템이 viewModel로 제대로 binding되는지 확인하기 위함), SearchTextField에 입력 된 text viewModel로 binding

--- Book2OnNoN/Source/Views/Search/SearchViewController.swift
+++ Book2OnNoN/Source/Views/Search/SearchViewController.swift
@@ -6,24 +6,112 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+import DropDown
 
-class SearchViewController: UIViewController {
+class SearchViewController: BaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private let viewModel: SearchViewModelType
+    
+    init(viewModel: SearchViewModelType = SearchViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    private lazy var searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "검색어를 입력하세요."
+        textField.borderStyle = .none
+        return textField
+    }()
+    
+    private lazy var searchDropDownButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("제목", for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private lazy var searchOptionDropDown: DropDown = {
+         let dropDown = DropDown()
+         dropDown.anchorView = searchDropDownButton
+         return dropDown
+     }()
 
+    private var selectedSearchOption: String?
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        searchTextField.layer.addBorder(edge: .bottom, color: .black, thickness: 2.0)
+    }
+    
+    override func setViewController() {
+        view.backgroundColor = .white
+    }
+    
+    override func setAddViews() {
+        [searchTextField, searchDropDownButton].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        searchTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.leading.equalToSuperview().offset(70)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(50)
+        }
+        
+        searchDropDownButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(10)
+            $0.trailing.equalTo(searchTextField.snp.leading)
+            $0.height.equalTo(50)
+        }
+    }
+    
+    override func bind() {
+        // Input
+        searchDropDownButton.rx.tap
+            .asObservable()
+            .bind(to: viewModel.searchDropDownButtonTapped)
+            .disposed(by: disposeBag)
+        
+        searchTextField.rx.text.orEmpty
+            .asObservable()
+            .bind(to: viewModel.searchTextFieldInputTextValue)
+            .disposed(by: disposeBag)
+        
+        searchOptionDropDown.rx.selectionAction.onNext { [weak self] index, item in
+            self?.searchDropDownButton.setTitle(item, for: .normal)
+            self?.viewModel.whichSelectedDropDownItem.onNext(item)
+        }
+            
+        // Output
+        viewModel.resultDownButtonTapped
+            .asDriver()
+            .drive(onNext: {[weak self] data in
+                self?.searchOptionDropDown.show()
+                self?.searchOptionDropDown.dataSource = data
+            }).disposed(by: disposeBag)
+        
+        viewModel.resultSearch
+            .asDriver()
+            .drive(onNext: { t in
+                print(t)
+            }).disposed(by: disposeBag)
+    }
+    
+    private func showDropDown() {
+        searchOptionDropDown.show()
+    }
 }
+
+
