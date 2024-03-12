81c2d5f84c31cc03676de24d0bfe1e1e6644b661
YeoSeongil <seongil_yeo@naver.com>
2024-03-12 13:17:51 +0900

    [TEST] TEST BookService 책의 제목과 검색 타입을 입력 받아 검색하는 기능 테스트

--- Book2OnNoN/Configuration/Network/Service/BookService.swift
+++ Book2OnNoN/Configuration/Network/Service/BookService.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import RxSwift
+import Alamofire
+
+public struct BookService {
+    func paramTest(title: String, type:String) -> Observable<Result<Book, Error>>{
+        return Observable.create { observer -> Disposable in
+            AF.request(BookAPI.searchBook(title: title, type: type))
+                .responseDecodable(of: Book.self) { response in
+                    switch response.result {
+                    case .success(let data):
+                        observer.onNext(.success(data))
+                    case .failure(let error):
+                        observer.onError(error)
+                    }
+                }
+            return Disposables.create()
+        }
+    }
+}
