bfa8ba046a47cf84a1e87fc125da084dd0d481d5
YeoSeongil <seongil_yeo@naver.com>
2024-03-12 13:40:41 +0900

    [FEAT] FEAT BookRepository 책 검색 결과를 저장할 Repository 구현 : API 서버와 통신하여 책 검색 결과를 저장하는 getBookData 메소드 구현

--- Book2OnNoN/Configuration/Network/Repository/BookRepository.swift
+++ Book2OnNoN/Configuration/Network/Repository/BookRepository.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import RxSwift
+
+protocol BookRepositoryProtocol {
+    func getBookData(title: String, type: String) -> Single<Book>
+}
+
+class BookRepository: BookRepositoryProtocol {
+    static let shared = BookRepository()
+    
+    private let disposeBag = DisposeBag()
+    
+    private init() {}
+    
+    func getBookData(title: String, type: String) -> Single<Book> {
+        return BookService().getRequestForBookSearch(title: title, type: type)
+            .flatMap { result -> Single<Book> in
+                switch result {
+                case .success(let book):
+                    return .just(book)
+                case .failure(let error):
+                    return .error(error)
+                }
+            }
+    }
+}
