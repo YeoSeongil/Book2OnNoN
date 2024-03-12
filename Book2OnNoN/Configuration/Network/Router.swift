4b9d94457891cf1819cd48b48cf793fe1d3c0102
YeoSeongil <seongil_yeo@naver.com>
2024-03-12 13:41:41 +0900

    [FEAT] FEAT Router Router 프로토콜 구현

--- Book2OnNoN/Configuration/Network/Router.swift
+++ Book2OnNoN/Configuration/Network/Router.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import Alamofire
+
+public protocol Router {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String : String]? { get }
+    var parameters: [String : Any]? { get }
+    var encoding: ParameterEncoding? { get }
+}
