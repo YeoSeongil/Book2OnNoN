4734a77ad258f410f2278a494402d85d8c26dde3
YeoSeongil <seongil_yeo@naver.com>
2024-03-13 21:15:23 +0900

    [ADD] ADD SearchResultTableViewCell

---
+++ Book2OnNoN/Source/Views/Search/Cell/SearchResultTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultTableViewCell.swift
+//  Book2OnNoN
+//
+//  Created by 여성일 on 3/13/24.
+//
+
+import UIKit
+
+class SearchResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
