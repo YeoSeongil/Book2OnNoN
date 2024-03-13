c8547fff9b1351c8d57e8e85f61e92830239aba0
YeoSeongil <seongil_yeo@naver.com>
2024-03-14 00:28:09 +0900

    [TEST] TEST SearchDetailViewController Item 값을 제대로 받아와 Bind되는지 테스트 코드 작성

--- Book2OnNoN/Source/Views/Search/SearchDetailViewController.swift
+++ Book2OnNoN/Source/Views/Search/SearchDetailViewController.swift
@@ -6,24 +6,61 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
 
-class SearchDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class SearchDetailViewController: BaseViewController {
+    
+    private let viewModel: SearchDetailViewModelType
+    // MARK: UI Components
+    
+    private lazy var testLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        return label
+    }()
+    
+    // MARK: init
+    init(viewModel: SearchDetailViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    // MARK: SetUp ViewController
+    override func setViewController() {
+        super.setViewController()
+        view.backgroundColor = .blue
+    }
+    
+    override func setAddViews() {
+        super.setAddViews()
+        [testLabel].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        super.setConstraints()
+        testLabel.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(100)
+            $0.height.equalTo(100)
+        }
+    }
+    
+    override func bind() {
+        super.bind()
+        viewModel.resultDetailItem
+            .drive(onNext: { [weak self] item in
+                guard let item = item else { return }
+                self?.testLabel.text = item.title
+            })
+            .disposed(by: disposeBag)
+    }
 }
