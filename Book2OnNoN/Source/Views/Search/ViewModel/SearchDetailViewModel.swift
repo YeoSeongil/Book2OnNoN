1a15525e839e08f17ce8ceb0cbb10a294eee9bb5
YeoSeongil <seongil_yeo@naver.com>
2024-03-14 00:25:31 +0900

    [FEAT] FEAT SearchDetailViewModel item을 받아와 resultDetailItem에 accept하도록 ViewModel 구현

--- Book2OnNoN/Source/Views/Search/ViewModel/SearchDetailViewModel.swift
+++ Book2OnNoN/Source/Views/Search/ViewModel/SearchDetailViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+protocol SearchDetailViewModelType {
+    // Output
+    var resultDetailItem: Driver<Item?> { get }
+}
+
+class SearchDetailViewModel {
+    private let disposeBag = DisposeBag()
+    private let item: Item
+    
+    // Output
+    var outputDetailItem = BehaviorRelay<Item?>(value: nil)
+    
+    init(item: Item) {
+        self.item = item
+        outputDetailItem.accept(item)
+    }
+}
+
+extension SearchDetailViewModel: SearchDetailViewModelType {
+    var resultDetailItem: Driver<Item?> {
+        outputDetailItem.asDriver()
+    }
+}
