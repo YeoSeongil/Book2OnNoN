66d170f9e3fbe1c21abd2c1ce56edeb501ac4022
YeoSeongil <seongil_yeo@naver.com>
2024-03-13 22:37:02 +0900

    [FEAT] FEAT SearchResultTableViewCell UI Components 구현, Model의 Item으로 UI Components를 구성하는 Configuration 구현 (TableView 바인딩 시 사용)

--- Book2OnNoN/Source/Views/Search/Cell/SearchResultTableViewCell.swift
+++ Book2OnNoN/Source/Views/Search/Cell/SearchResultTableViewCell.swift
@@ -6,18 +6,98 @@
 //
 
 import UIKit
+import SnapKit
 
 class SearchResultTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let id: String = "SearchResultTableViewCell"
+    
+    // MARK: UI Components
+    private let bookThumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private let bookTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .Pretendard.cellBookTitle
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private let bookAuthorLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .Pretendard.medium
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private let bookPublisherLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray
+        label.font = .Pretendard.medium
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    // MARK: init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setCell()
+        setAddViews()
+        setConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: SetUp Cell
+    private func setCell() {
+        backgroundColor = .clear
+        selectionStyle = .none
+    }
+    
+    private func setAddViews() {
+        [bookThumbnailImageView, bookTitleLabel, bookAuthorLabel, bookPublisherLabel].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func setConstraint() {
+        bookThumbnailImageView.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview().inset(5)
+            $0.leading.equalToSuperview().offset(5)
+            $0.width.equalTo(70)
+        }
+        
+        bookTitleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(5)
+            $0.leading.equalTo(bookThumbnailImageView.snp.trailing).offset(10)
+            $0.trailing.equalToSuperview().inset(5)
+        }
+        
+        bookAuthorLabel.snp.makeConstraints {
+            $0.top.equalTo(bookTitleLabel.snp.bottom).offset(5)
+            $0.leading.equalTo(bookThumbnailImageView.snp.trailing).offset(10)
+            $0.trailing.equalToSuperview().inset(5)
+        }
+        
+        bookPublisherLabel.snp.makeConstraints {
+            $0.top.equalTo(bookAuthorLabel.snp.bottom).offset(5)
+            //$0.bottom.equalToSuperview().inset(5)
+            $0.leading.equalTo(bookThumbnailImageView.snp.trailing).offset(10)
+            $0.trailing.equalToSuperview().inset(5)
+        }
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    // MARK: Configuration
+    func configuration(book: Item) {
+        bookThumbnailImageView.setImageKingfisher(with: book.cover)
+        bookTitleLabel.text = book.title
+        bookAuthorLabel.text = book.author
+        bookPublisherLabel.text = book.publisher
     }
-
 }
