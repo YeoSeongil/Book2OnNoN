3a5024bc3bccb466f88990c141e7a9d7fded43f2
YeoSeongil <seongil_yeo@naver.com>
2024-03-13 22:39:42 +0900

    [CHORE] CHORE Font + Extension 사용 방식에 따른 Font 구분

--- Book2OnNoN/Configuration/Extension/Font + Extension.swift
+++ Book2OnNoN/Configuration/Extension/Font + Extension.swift
@@ -28,11 +28,15 @@ extension UIFont {
             case title = 27.0
         }
         
+        // Separate for Size
         static let light = UIFont(name: Pretendard.name.light.rawValue, size: Pretendard.Size.light.rawValue)
         static let medium = UIFont(name: Pretendard.name.medium.rawValue, size: Pretendard.Size.medium.rawValue)
         static let regular = UIFont(name: Pretendard.name.regular.rawValue, size: Pretendard.Size.regular.rawValue)
         static let semibold = UIFont(name: Pretendard.name.semibold.rawValue, size: Pretendard.Size.semibold.rawValue)
         static let bold = UIFont(name: Pretendard.name.bold.rawValue, size: Pretendard.Size.bold.rawValue)
         static let title = UIFont(name: Pretendard.name.bold.rawValue, size: Pretendard.Size.title.rawValue)
+        
+        // Separate for Use
+        static let cellBookTitle = UIFont(name: Pretendard.name.bold.rawValue, size: 15.0)
     }
 }
