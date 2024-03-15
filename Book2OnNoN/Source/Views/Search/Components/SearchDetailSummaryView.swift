23e4a876ae1ac67532cac58ce2fcdf055f4de828
YeoSeongil <seongil_yeo@naver.com>
2024-03-15 19:32:34 +0900

    [FEAT] FEAT SearchDetailSummaryView UI Component 구현 SearchDetailSummaryView의 UI에 Bind 할 수 있는 configuration Method 구현

--- Book2OnNoN/Source/Views/Search/Components/SearchDetailSummaryView.swift
+++ Book2OnNoN/Source/Views/Search/Components/SearchDetailSummaryView.swift
@@ -6,15 +6,78 @@
 //
 
 import UIKit
+import SnapKit
 
 class SearchDetailSummaryView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: UIComponents
+    private let detailBookThumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .clear
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
+    
+    private let detailBookTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .Pretendard.semibold
+        label.numberOfLines = 3
+        label.textColor = .black
+        return label
+    }()
+        
+    private let detailBookAuthorLabel: UILabel = {
+        let label = UILabel()
+        label.font = .Pretendard.regular
+        label.numberOfLines = 3
+        label.textColor = .gray
+        return label
+    }()
+    
+    
+    // MARK: init
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+        setView()
+        setConfiguration()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Set View
+    private func setView() {
+        [detailBookThumbnailImageView, detailBookTitleLabel, detailBookAuthorLabel].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func setConfiguration() {
+        detailBookThumbnailImageView.snp.makeConstraints {
+            $0.width.equalTo(100)
+            $0.height.equalToSuperview()
+            $0.centerY.equalTo(safeAreaLayoutGuide)
+            $0.leading.equalTo(safeAreaLayoutGuide)
+        }
+        
+        detailBookTitleLabel.snp.makeConstraints {
+            $0.bottom.equalTo(detailBookAuthorLabel.snp.top).offset(-10)
+            $0.leading.equalTo(detailBookThumbnailImageView.snp.trailing).offset(10)
+            $0.trailing.equalTo(safeAreaInsets)
+        }
+        
+        detailBookAuthorLabel.snp.makeConstraints {
+            $0.bottom.equalTo(safeAreaInsets)
+            $0.leading.equalTo(detailBookThumbnailImageView.snp.trailing).offset(10)
+            $0.trailing.equalTo(safeAreaInsets)
+        }
+    }
+    
+    // MARK: Method
+    func configuration(with item: Item) {
+        detailBookThumbnailImageView.setImageKingfisher(with: item.cover)
+        detailBookTitleLabel.text = item.title
+        detailBookAuthorLabel.text = item.author
+    }
 }
