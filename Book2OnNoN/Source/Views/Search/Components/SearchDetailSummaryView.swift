d0f2c6b8eb6f812b9f3e32a902faf624b0a2e944
YeoSeongil <seongil_yeo@naver.com>
2024-03-15 18:57:22 +0900

    [ADD] ADD SearchDetailSummaryView SearchDetailViewController의 요약 View 생성

---
+++ Book2OnNoN/Source/Views/Search/Components/SearchDetailSummaryView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchDetailSummaryView.swift
+//  Book2OnNoN
+//
+//  Created by 여성일 on 3/15/24.
+//
+
+import UIKit
+
+class SearchDetailSummaryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
