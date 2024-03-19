4b0a5cbf024bed46a892265dd74ce13a105ce09a
YeoSeongil <seongil_yeo@naver.com>
2024-03-19 23:15:20 +0900

    [FEAT, TEST] FEAT, TEST SearchDetailRecordButtonView FEAT : UI Component구현 TEST : Delegate 패턴 테스트 : Button Tapped시 상위 Vc에서 이벤트 처리를 위해 델리게이트 패턴으로 이벤트 전송 테스팅

--- Book2OnNoN/Source/Views/Search/Components/SearchDetailRecordButtonView.swift
+++ Book2OnNoN/Source/Views/Search/Components/SearchDetailRecordButtonView.swift
@@ -6,15 +6,101 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+protocol SearchDetailRecordButtonViewDelegate: AnyObject {
+    func didTappedRecordFinishedReadingButton()
+    func didTappedRecordReadingButton()
+    func didrecordInterestedButton()
+}
 
 class SearchDetailRecordButtonView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    weak var delegate: SearchDetailRecordButtonViewDelegate?
+    private let disposeBag = DisposeBag()
+    
+    // MARK: UIComponents
+    private let recordButtonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    private let recordFinishedReadingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("다 읽은 책", for: .normal)
+        button.titleLabel?.font = UIFont.Pretendard.semibold
+        button.tintColor = .white
+        button.layer.cornerRadius = 5
+        button.backgroundColor = .PrestigeBlue
+        return button
+    }()    
+    
+    private let recordReadingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("읽고 있는 책", for: .normal)
+        button.titleLabel?.font = UIFont.Pretendard.semibold
+        button.tintColor = .white
+        button.layer.cornerRadius = 5
+        return button
+    }()    
+    
+    private let recordInterestedButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("관심 있는 책", for: .normal)
+        button.titleLabel?.font = UIFont.Pretendard.semibold
+        button.tintColor = .white
+        button.layer.cornerRadius = 5
+        return button
+    }()
 
+    // MARK: init
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+        setView()
+        setConfiguration()
+        bind()
+    }
+    
+    required init?(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Set View
+    private func setView() {
+        [recordButtonStackView].forEach {
+            addSubview($0)
+        }
+        
+        [recordFinishedReadingButton, recordReadingButton, recordInterestedButton].forEach {
+            recordButtonStackView.addArrangedSubview($0)
+        }
+    }
+    
+    private func setConfiguration() {
+        recordButtonStackView.snp.makeConstraints {
+            $0.verticalEdges.equalTo(safeAreaLayoutGuide)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide)
+        }
+    }
+    
+    private func bind() {
+        recordFinishedReadingButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.delegate?.didTappedRecordFinishedReadingButton()
+            }).disposed(by: disposeBag)
+        
+        recordReadingButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.delegate?.didTappedRecordReadingButton()
+            }).disposed(by: disposeBag)
+        
+        recordInterestedButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.delegate?.didrecordInterestedButton()
+            }).disposed(by: disposeBag)
+    }
 }
