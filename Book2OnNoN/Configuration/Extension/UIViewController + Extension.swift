2ba37f82c50bc4244bc738614ec40f08dc9c0925
YeoSeongil <seongil_yeo@naver.com>
2024-03-30 13:57:18 +0900

    [FEAT] FEAT UIViewController + Extension 확인 버튼만 클릭할 수 있는 Alert를 Show하는 showOnlyOkAlert 구현

--- Book2OnNoN/Configuration/Extension/UIViewController + Extension.swift
+++ Book2OnNoN/Configuration/Extension/UIViewController + Extension.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    /// 확인 버튼만 클릭할 수 있는 Alert를 Show 합니다.
+    /// ```
+    /// UIViewController.showOnlyOkAlert(title: "경고", message: "잘못 되었습니다.", buttonTitle: "확인")
+    /// ```
+    /// - Parameters:
+    ///   - title: Alert의 Title Text
+    ///   - message: Alert의 Message Text
+    ///   - buttonTitle: Alert의 Button Title
+    func showOnlyOkAlert(title: String, message: String, buttonTitle: String) {
+        let alertSheet = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertSheet.addAction(UIAlertAction(title: buttonTitle, style: .default))
+        self.present(alertSheet, animated: true)
+    }
+}
