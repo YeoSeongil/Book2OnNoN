6aa633bc4a3432c2a78f0b5b86f06b7531d32439
YeoSeongil <seongil_yeo@naver.com>
2024-03-30 13:39:42 +0900

    [FEAT] FEAT UIView + Extension 진동 애니메이션 (좌우로 흔들리는 애니메이션) 구현

--- Book2OnNoN/Configuration/Extension/UIView + Extension.swift
+++ Book2OnNoN/Configuration/Extension/UIView + Extension.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView {
+    func shakeAnimation() {
+        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        animation.duration = 0.4
+        animation.values = [-5.0, 5.0, -5.0, 5.0, -5.0, 5.0, -2.5, 2.5, 0.0 ]
+        layer.add(animation, forKey: "shake")
+    }
+}
