bdba704ccac02ddbf4cd271eaeede31b0e950149
YeoSeongil <seongil_yeo@naver.com>
2024-04-01 02:04:57 +0900

    [FEAT] FEAT CoreDataManager CoreDataManager 기능 구현

--- Book2OnNoN/Configuration/CoreData/CoreDataManager.swift
+++ Book2OnNoN/Configuration/CoreData/CoreDataManager.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataManager {
+    static let shared: CoreDataManager = CoreDataManager()
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Book2OnNoN")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("error \(error)")
+            }
+        })
+        return container
+    }()
+    
+    private var managedObjectContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    func saveData() {
+        guard managedObjectContext.hasChanges else { return }
+        do {
+            try managedObjectContext.save()
+            print("세이브 성공")
+        } catch {
+            print("save Error")
+        }
+    }
+    
+    func insertData<T: NSManagedObject>(_ type: T.Type) -> T? {
+        guard let entityName = T.entity().name else {
+            return nil
+        }
+        print(entityName)
+        return NSEntityDescription.insertNewObject(forEntityName: entityName, into: managedObjectContext) as? T
+    }
+    
+    func deleteData(_ object: NSManagedObject) {
+        managedObjectContext.delete(object)
+    }
+    
+    func fetchData<T: NSManagedObject>(_ type: T.Type) -> [T]? {
+        let fetchRequest = NSFetchRequest<T>(entityName: String(describing: type))
+        do {
+            let result = try managedObjectContext.fetch(fetchRequest)
+            return result
+        } catch {
+            print("fetch Error")
+            return nil
+        }
+    }
+}
