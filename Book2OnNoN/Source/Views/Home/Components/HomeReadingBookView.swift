99545cbc719ea8f52a0d667ab2b82d0cd456f601
YeoSeongil <seongil_yeo@naver.com>
2024-04-09 13:18:02 +0900

    [ADD] HomeReadingBookView

---
+++ Book2OnNoN/Source/Views/Home/Components/HomeReadingBookView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeReadingBookView.swift
+//  Book2OnNoN
+//
+//  Created by 여성일 on 4/9/24.
+//
+
+import UIKit
+
+class HomeReadingBookView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
