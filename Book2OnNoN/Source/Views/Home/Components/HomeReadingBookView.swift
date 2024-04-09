6e36fcc796ac7b4c64e4e09405b978788e6cf01a
YeoSeongil <seongil_yeo@naver.com>
2024-04-09 14:57:39 +0900

    [FEAT] FEAT HomeReadingBookView UI Component, CollectionView에 데이터 바인딩 구현

--- Book2OnNoN/Source/Views/Home/Components/HomeReadingBookView.swift
+++ Book2OnNoN/Source/Views/Home/Components/HomeReadingBookView.swift
@@ -6,15 +6,82 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
 class HomeReadingBookView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    private let disposeBag = DisposeBag()
+    private let viewModel: HomeViewModelType
+    
+    // MARK: UI Components
+    private let homeReadingBookViewLabel: UILabel = {
+        let label = UILabel()
+        label.text = "읽고 있는 책"
+        label.textColor = .white
+        label.font = .Pretendard.semibold
+        return label
+    }()
+    
+    private let readingBookCollectionView: UICollectionView  = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(HomeReadingBookCollectionViewCell.self, forCellWithReuseIdentifier:HomeReadingBookCollectionViewCell.id)
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+    
+    // MARK: init
+    init(viewModel: HomeViewModelType) {
+        self.viewModel = viewModel
+        super.init(frame: .zero)
+        setView()
+        setConstraints()
+        bind()
     }
-    */
 
+    required init?(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Set View
+    private func setView() {
+        [homeReadingBookViewLabel, readingBookCollectionView].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func setConstraints() {
+        homeReadingBookViewLabel.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.height.equalTo(30)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide)
+        }
+        
+        readingBookCollectionView.snp.makeConstraints {
+            $0.top.equalTo(homeReadingBookViewLabel.snp.bottom).offset(10)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide)
+            $0.bottom.equalTo(safeAreaLayoutGuide)
+        }
+    }
+    
+    private func bind() {
+        readingBookCollectionView.rx.setDelegate(self).disposed(by: disposeBag)
+        
+        // Output
+        viewModel.resultReadingBookRecordItem
+            .drive(readingBookCollectionView.rx.items(cellIdentifier: HomeReadingBookCollectionViewCell.id, cellType: HomeReadingBookCollectionViewCell.self)) { row, item, cell in
+                cell.configuration(book: item)
+            }.disposed(by: disposeBag)
+    }
+}
+
+extension HomeReadingBookView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = collectionView.bounds.width
+        let hegiht: CGFloat = collectionView.bounds.height
+        return CGSize(width: width, height: hegiht)
+    }
 }
