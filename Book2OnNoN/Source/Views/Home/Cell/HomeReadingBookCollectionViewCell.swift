823e23a1ab49a6507028e4279d07864e1cae0133
YeoSeongil <seongil_yeo@naver.com>
2024-04-09 14:57:02 +0900

    [ADD] ADD HomeReadingBookCollectionViewCell

---
+++ Book2OnNoN/Source/Views/Home/Cell/HomeReadingBookCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeReadingBookCollectionViewCell.swift
+//  Book2OnNoN
+//
+//  Created by 여성일 on 4/9/24.
+//
+
+import UIKit
+
+class HomeReadingBookCollectionViewCell: UICollectionViewCell {
+    
+}
