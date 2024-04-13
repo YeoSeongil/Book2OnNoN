1986ae714356a5bed4b2a992509f10b5968f31c4
YeoSeongil <seongil_yeo@naver.com>
2024-04-13 21:44:53 +0900

    [ADD] ADD HomeInterestedBookView, HomeInterestedBookCollectinViewCell

---
+++ Book2OnNoN/Source/Views/Home/Cell/HomeInterestedBookCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HomeInterestedBookCollectionViewCell.swift
+//  Book2OnNoN
+//
+//  Created by 여성일 on 4/13/24.
+//
+
+import UIKit
+
+class HomeInterestedBookCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Book2OnNoN/Source/Views/Home/Components/HomeInterestedBookView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeInterestedBookView.swift
+//  Book2OnNoN
+//
+//  Created by 여성일 on 4/13/24.
+//
+
+import UIKit
+
+class HomeInterestedBookView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
