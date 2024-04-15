11de47b27d1f266ef6284bada223a68f7aacfa82
YeoSeongil <seongil_yeo@naver.com>
2024-04-15 15:38:31 +0900

    [FEAT] FEAT RegisterViewModel ViewModel Type 프로토콜 설계, 에러 처리용 enum, 텍스트 필드 분기 처리용 Enum 구현 입력 받은 유저 정보를 CoreData에 저장하는 trySaveUserData() 구현

--- Book2OnNoN/Source/Views/Register/ViewModel/RegisterViewModel.swift
+++ Book2OnNoN/Source/Views/Register/ViewModel/RegisterViewModel.swift
@@ -6,3 +6,92 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+enum RegisterError {
+    case emptyRegisterTextField
+}
+
+enum RegisterSaveProcedureType  {
+    case successSave
+    case failureSave
+}
+
+protocol RegisterViewModelType {
+    // Input
+    var didRegisterButtonTapped: AnyObserver<Void> { get }
+    var didRegisterUserName: AnyObserver<String> { get }
+
+    // Output
+    var resultRegisterError: Driver<RegisterError> { get }
+    var resultRegisterSaveProcedureType: Driver<RegisterSaveProcedureType> { get }
+}
+
+class RegisterViewModel {
+    
+    private let disposeBag = DisposeBag()
+    // Input
+    private let inputDidRegisterButtonTapped = PublishSubject<Void>()
+    private let inputRegisterUserName = PublishSubject<String>()
+    
+    // Output
+    private let outputRegisterError = PublishRelay<RegisterError>()
+    private let outputRegisterSaveProcedureType = PublishRelay<RegisterSaveProcedureType>()
+    
+    init() {
+        trySaveUserData()
+    }
+    
+    private func trySaveUserData() {
+        inputDidRegisterButtonTapped
+            .withLatestFrom(inputRegisterUserName)
+            .subscribe(onNext: { name in
+                if name.isEmpty {
+                    self.outputRegisterError.accept(.emptyRegisterTextField)
+                } else {
+                    if let data = CoreDataManager.shared.insertData(Book2OnNonUser.self) {
+                        data.isRegister = true
+                        data.name = name
+                        
+                        CoreDataManager.shared.saveData { result in
+                            switch result {
+                            case .success:
+                                self.outputRegisterSaveProcedureType.accept(.successSave)
+                            case .failure(let errorr):
+                                self.outputRegisterSaveProcedureType.accept(.failureSave)
+                            }
+                        }
+                    }
+                }
+            }).disposed(by: disposeBag)
+    }
+    
+    private func handleRegisterResult(_ result: Book) {
+    }
+    
+    
+}
+
+extension RegisterViewModel: RegisterViewModelType {
+    // Input
+    var didRegisterButtonTapped: AnyObserver<Void> {
+        inputDidRegisterButtonTapped.asObserver()
+    }
+    
+    var didRegisterUserName: AnyObserver<String> {
+        inputRegisterUserName.asObserver()
+    }
+    
+    // Output
+    var resultRegisterError: Driver<RegisterError> {
+        outputRegisterError.asDriver(onErrorDriveWith: .empty())
+    }
+    
+    var resultRegisterSaveProcedureType: Driver<RegisterSaveProcedureType> {
+        outputRegisterSaveProcedureType.asDriver(onErrorDriveWith: .empty())
+    }
+
+    
+}
+
