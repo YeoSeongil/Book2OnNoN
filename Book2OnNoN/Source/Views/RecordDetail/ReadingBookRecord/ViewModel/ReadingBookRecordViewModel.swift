1c1a70164d7355ce03ae7abcb50ef3b4b5243a93
YeoSeongil <seongil_yeo@naver.com>
2024-04-16 20:42:08 +0900

    [FEAT] FEAT ReadingBookRecordViewModel 주입 받은 데이터 output구현, 프로토콜 구현

--- Book2OnNoN/Source/Views/RecordDetail/ReadingBookRecord/ViewModel/ReadingBookRecordViewModel.swift
+++ Book2OnNoN/Source/Views/RecordDetail/ReadingBookRecord/ViewModel/ReadingBookRecordViewModel.swift
@@ -6,20 +6,29 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 protocol ReadingBookRecordViewModelType {
-    
+    // Output
+    var ResultReadingBooksRecordData: Driver<[ReadingBooks]> { get }
 }
 
 class ReadingBookRecordViewModel {
     private let readingBookRecordData: ReadingBooks
     
+    // Output
+    private let outputReadingBooksRecordData = BehaviorRelay<[ReadingBooks]>(value: [])
+    
     init(readingBookRecordData: ReadingBooks) {
         self.readingBookRecordData = ReadingBooks()
+        outputReadingBooksRecordData.accept([readingBookRecordData])
     }
 
 }
 
 extension ReadingBookRecordViewModel: ReadingBookRecordViewModelType {
-    
+    var ResultReadingBooksRecordData: Driver<[ReadingBooks]> {
+        outputReadingBooksRecordData.asDriver(onErrorDriveWith: .empty())
+    }
 }
