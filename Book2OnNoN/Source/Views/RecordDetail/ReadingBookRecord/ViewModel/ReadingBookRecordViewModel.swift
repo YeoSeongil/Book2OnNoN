06d67d42dcac6559912d4296fae5377b39c171ab
YeoSeongil <seongil_yeo@naver.com>
2024-04-16 21:12:35 +0900

    [FEAT] FEAT ReadingBookRecordViewModel 삭제 구현, 삭제 분기처리

--- Book2OnNoN/Source/Views/RecordDetail/ReadingBookRecord/ViewModel/ReadingBookRecordViewModel.swift
+++ Book2OnNoN/Source/Views/RecordDetail/ReadingBookRecord/ViewModel/ReadingBookRecordViewModel.swift
@@ -9,26 +9,67 @@ import Foundation
 import RxSwift
 import RxCocoa
 
+enum ReadingBookRecordDeleteProcedureType  {
+    case successDelete
+    case failureDelete
+}
+
 protocol ReadingBookRecordViewModelType {
+    // Input
+    var didDeleteButtonTapped: AnyObserver<Void> { get }
+    
     // Output
-    var ResultReadingBooksRecordData: Driver<[ReadingBooks]> { get }
+    var resultReadingBooksRecordData: Driver<[ReadingBooks]> { get }
+    var resultReadingBookRecordDeleteProcedureType: Driver<ReadingBookRecordDeleteProcedureType> { get }
 }
 
 class ReadingBookRecordViewModel {
+    private let disposBag = DisposeBag()
     private let readingBookRecordData: ReadingBooks
+    
+    // Input
+    private let inputDeleteButtonTapped = PublishSubject<Void>()
     
     // Output
     private let outputReadingBooksRecordData = BehaviorRelay<[ReadingBooks]>(value: [])
+    private let outputReadingBookRecordDeleteProcedureType = PublishRelay<ReadingBookRecordDeleteProcedureType>()
     
     init(readingBookRecordData: ReadingBooks) {
-        self.readingBookRecordData = ReadingBooks()
+        self.readingBookRecordData = readingBookRecordData
         outputReadingBooksRecordData.accept([readingBookRecordData])
+        tryDeleteReadingBookRecord()
     }
 
+    private func tryDeleteReadingBookRecord() {
+        inputDeleteButtonTapped
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                CoreDataManager.shared.deleteData(self.readingBookRecordData)
+                CoreDataManager.shared.saveData { result in
+                    switch result {
+                    case .success:
+                        self.outputReadingBookRecordDeleteProcedureType.accept(.successDelete)
+                    case .failure(let error):
+                        self.outputReadingBookRecordDeleteProcedureType.accept(.failureDelete)
+                    }
+                }
+            })
+            .disposed(by: disposBag)
+    }
 }
 
 extension ReadingBookRecordViewModel: ReadingBookRecordViewModelType {
-    var ResultReadingBooksRecordData: Driver<[ReadingBooks]> {
+    // Input
+    var didDeleteButtonTapped: AnyObserver<Void> {
+        inputDeleteButtonTapped.asObserver()
+    }
+    
+    // Output
+    var resultReadingBooksRecordData: Driver<[ReadingBooks]> {
         outputReadingBooksRecordData.asDriver(onErrorDriveWith: .empty())
+    }
+    
+    var resultReadingBookRecordDeleteProcedureType: Driver<ReadingBookRecordDeleteProcedureType> {
+        outputReadingBookRecordDeleteProcedureType.asDriver(onErrorDriveWith: .empty())
     }
 }
