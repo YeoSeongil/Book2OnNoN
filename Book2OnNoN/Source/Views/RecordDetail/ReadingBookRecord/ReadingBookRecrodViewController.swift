94dfcdd6cdde8d872e170b31947632c549ac48ca
YeoSeongil <seongil_yeo@naver.com>
2024-04-16 20:43:05 +0900

    [FEAT] FEAT ReadingBookRecordViewController UI Components구현, 제약조건 및 View Setup,

--- Book2OnNoN/Source/Views/RecordDetail/ReadingBookRecord/ReadingBookRecrodViewController.swift
+++ Book2OnNoN/Source/Views/RecordDetail/ReadingBookRecord/ReadingBookRecrodViewController.swift
@@ -6,24 +6,54 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import CoreData
 
-class ReadingBookRecrodViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class ReadingBookRecordViewController: BaseViewController {
+    
+    private let viewModel: ReadingBookRecordViewModelType
+    
+    private lazy var readingBookRecordSummaryView = ReadingBookRecordSummaryView(viewModel: viewModel)
+    // MARK: UI Components
+    
+    // MARK: init
+    init(viewModel: ReadingBookRecordViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    // MARK: SetUp ViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setViewController() {
+        super.setViewController()
+        [readingBookRecordSummaryView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        super.setConstraints()
+        readingBookRecordSummaryView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview().inset(10)
+            $0.height.equalTo(200)
+        }
+    }
+    
+    override func setNavigation() {
+        super.setNavigation()
+    }
+    
+    override func bind() {
+        super.bind()
+    }
 }
