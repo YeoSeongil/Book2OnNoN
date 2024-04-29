1a3f0dfb14711cb108e1f42bce25d8a7a60b7bc6
YeoSeongil <seongil_yeo@naver.com>
2024-04-29 16:41:03 +0900

    [ADD] ADD ReadingBookRecordContentView

---
+++ Book2OnNoN/Source/Views/RecordDetail/ReadingBookRecord/Components/ReadingBookRecordContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ReadingBookRecordContentView.swift
+//  Book2OnNoN
+//
+//  Created by 여성일 on 4/29/24.
+//
+
+import UIKit
+
+class ReadingBookRecordContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
