e77bdc351bacbe1ae829d032680288c00ab78226
YeoSeongil <seongil_yeo@naver.com>
2024-05-07 18:31:48 +0900

    [FEAT] FEAT Rx + Extension UILabel에 Tap 이벤트 추가

--- Book2OnNoN/Configuration/Extension/Rx + Extension.swift
+++ Book2OnNoN/Configuration/Extension/Rx + Extension.swift
@@ -5,4 +5,21 @@
 //  Created by 여성일 on 5/7/24.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: UILabel {
+    var tap: Observable<Void> {
+        let tapGestureRecognizer = UITapGestureRecognizer()
+        
+        base.addGestureRecognizer(tapGestureRecognizer)
+        base.isUserInteractionEnabled = true
+        
+        let source = tapGestureRecognizer.rx.event
+            .map { _ in () }
+            .asObservable()
+        
+        return source
+    }
+}
