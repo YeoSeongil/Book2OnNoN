407dca507150bfad4fdbafc0f1b50b0d8ed6f3ba
YeoSeongil <seongil_yeo@naver.com>
2024-05-08 13:47:02 +0900

    [FEAT] FEAT String + Extension 문자열에서 숫자만 추출하여 Int로 형변환 하여 반환하는 메소드 구햔

--- Book2OnNoN/Configuration/Extension/String + Extension.swift
+++ Book2OnNoN/Configuration/Extension/String + Extension.swift
@@ -5,4 +5,24 @@
 //  Created by 여성일 on 5/8/24.
 //
 
-import Foundation
+import UIKit
+
+extension String {
+    /// 문자열에서 숫자만 추출하여 `Int`로 변환합니다.
+    /// 변환이 실패하면 `nil`을 반환합니다.
+    /// ```
+    /// String.extractDigitsToInt()
+    /// ```
+    func extractDigitsToInt() -> Int? {
+
+        let digitCharacterSet = CharacterSet.decimalDigits
+        let numericString = self.unicodeScalars.filter {
+            digitCharacterSet.contains($0)
+        }
+        .map { Character($0) }
+        .map { String($0) }
+        .joined()
+        
+        return Int(numericString)
+    }
+}
