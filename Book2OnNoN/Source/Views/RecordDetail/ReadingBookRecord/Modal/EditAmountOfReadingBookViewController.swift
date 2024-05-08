d3c8980c06dc898af4bfe8bb76bbb8e05d8c2afe
YeoSeongil <seongil_yeo@naver.com>
2024-05-08 14:03:21 +0900

    [FEAT] FEAT EditAmountOfReadingBookViewController UIComponent 구현, ViewModel에 값 바인딩 기능 구현, 버튼 이벤트 처리 구현, ViewModel에서 값을 받아와 Picker의 범위, 초기값 설정 구현

--- Book2OnNoN/Source/Views/RecordDetail/ReadingBookRecord/Modal/EditAmountOfReadingBookViewController.swift
+++ Book2OnNoN/Source/Views/RecordDetail/ReadingBookRecord/Modal/EditAmountOfReadingBookViewController.swift
@@ -6,24 +6,155 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import CoreData
 
-class EditAmountOfReadingBookViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class EditAmountOfReadingBookViewController: BaseViewController {
+    private let viewModel: ReadingBookRecordViewModelType
+    
+    // MARK: UI Components
+    private let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .PrestigeBlue
+        view.layer.cornerRadius = 8.0
+        return view
+    }()
+    
+    private let modalTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "수정하기"
+        label.textColor  = .white
+        label.backgroundColor = .clear
+        label.textAlignment = .center
+        label.font = .Pretendard.semibold
+        return label
+    }()
+    
+    private let closeModalButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "xmark"), for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .clear
+        return button
+    }()
+    
+    private let amountOfReadingBookPicker: UIPickerView = {
+        let picker = UIPickerView()
+        return picker
+    }()
+    
+    private let saveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("저장하기", for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 8
+        button.backgroundColor = UIColor(white: 0, alpha: 0.3)
+        return button
+    }()
+    
+    // MARK: init
+    init(viewModel: ReadingBookRecordViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    // MARK: SetUp ViewController
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    override func setViewController() {
+        super.setViewController()
+        view.backgroundColor  = UIColor(white: 0, alpha: 0.5)
+        [containerView].forEach {
+            view.addSubview($0)
+        }
+        
+        [modalTitleLabel, closeModalButton, amountOfReadingBookPicker, saveButton].forEach {
+            containerView.addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        super.setConstraints()
+        containerView.snp.makeConstraints {
+            $0.centerX.equalTo(view.safeAreaLayoutGuide)
+            $0.centerY.equalTo(view.safeAreaLayoutGuide)
+            $0.width.equalTo(300)
+            $0.height.equalTo(250)
+        }
+        
+        modalTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(containerView.snp.top).offset(10)
+            $0.horizontalEdges.equalTo(containerView.snp.horizontalEdges).inset(50)
+        }
+        
+        closeModalButton.snp.makeConstraints {
+            $0.width.equalTo(20)
+            $0.height.equalTo(20)
+            $0.top.equalTo(containerView.snp.top).offset(10)
+            $0.trailing.equalTo(containerView.snp.trailing).offset(-10)
+        }
+        
+        amountOfReadingBookPicker.snp.makeConstraints {
+            $0.top.equalTo(containerView.snp.top).offset(50)
+            $0.bottom.equalTo(containerView.snp.bottom).offset(-50)
+            $0.horizontalEdges.equalTo(containerView.snp.horizontalEdges).inset(20)
+        }
+        
+        saveButton.snp.makeConstraints {
+            $0.bottom.equalTo(containerView.snp.bottom).offset(-10)
+            $0.horizontalEdges.equalTo(containerView.snp.horizontalEdges).inset(50)
+        }
+    }
+    
+    override func bind() {
+        super.bind()
+        closeModalButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.dismiss(animated: true)
+            })
+            .disposed(by: disposeBag)
 
+        // Input
+        saveButton.rx.tap
+            .bind(to: viewModel.didEditAmountOfReadingBookSaveButtonTapped)
+            .disposed(by: disposeBag)
+        
+        amountOfReadingBookPicker.rx.modelSelected(Int.self)
+            .map { selectRows in return selectRows.first ?? 1 }
+            .bind(to: viewModel.didEditAmountOfReadingBookValue)
+            .disposed(by: disposeBag)
+    
+        // Output
+        viewModel.resultReadingBookLookUpItem
+            .drive(onNext: { item in
+                Observable.just(Array(1...item[0].subInfo.itemPage))
+                    .bind(to: self.amountOfReadingBookPicker.rx.itemTitles) { _ , item in
+                        return "\(item)"
+                    }.disposed(by: self.disposeBag)
+            }).disposed(by: disposeBag)
+        
+        viewModel.resultReadingBooksRecordData
+            .drive(onNext: { data in
+                var rowIndex: Int
+                
+                if data[0].readingPage == 1 {
+                    rowIndex = 1
+                } else {
+                    rowIndex = Int(data[0].readingPage - 1)
+                }
+                
+                self.amountOfReadingBookPicker.selectRow(rowIndex, inComponent: 0, animated: false)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    // MARK: Method
 }
