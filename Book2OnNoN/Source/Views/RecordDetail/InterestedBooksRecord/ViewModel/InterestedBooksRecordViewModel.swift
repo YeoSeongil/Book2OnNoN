909b3cab0fc4c6b328821c56ce1ea3b19f200688
YeoSeongil <seongil_yeo@naver.com>
2024-05-11 20:11:49 +0900

    [FEAT] FEAT InterestedBookRecordViewModel 버튼 이벤트에 필요한 프로토콜 프로퍼티, 변수 추가

--- Book2OnNoN/Source/Views/RecordDetail/InterestedBooksRecord/ViewModel/InterestedBooksRecordViewModel.swift
+++ Book2OnNoN/Source/Views/RecordDetail/InterestedBooksRecord/ViewModel/InterestedBooksRecordViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+enum InterestedBookRecordDeleteProcedureType  {
+    case successDelete
+    case failureDelete
+}
+
+enum InterestedBookRecordEditProcedureType  {
+    case successEdit
+    case failureEdit
+}
+
+protocol InterestedBookRecordViewModelType {
+    // Input
+    var didDeleteButtonTapped: AnyObserver<Void> { get }
+}
+
+class InterestedBooksRecordViewModel {
+    private let interestedBookRecordData: InterestedReadingBooks
+    
+    private let inputDeleteButtonTapped = PublishSubject<Void>()
+    
+    init(interestedBookRecordData: InterestedReadingBooks) {
+        self.interestedBookRecordData = interestedBookRecordData
+    }
+}
+
+extension InterestedBooksRecordViewModel: InterestedBookRecordViewModelType {
+    // Input
+    var didDeleteButtonTapped: AnyObserver<Void> {
+        inputDeleteButtonTapped.asObserver()
+    }
+    
+}
