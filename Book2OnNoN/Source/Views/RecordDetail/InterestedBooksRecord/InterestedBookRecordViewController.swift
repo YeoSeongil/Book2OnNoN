57756358ecda35c57bb3382e47b7ed758d2c05b5
YeoSeongil <seongil_yeo@naver.com>
2024-05-11 20:09:34 +0900

    [FEAT] FEAT InterestedBookRecordViewController UI Components구현, ViewModel 주입

--- Book2OnNoN/Source/Views/RecordDetail/InterestedBooksRecord/InterestedBookRecordViewController.swift
+++ Book2OnNoN/Source/Views/RecordDetail/InterestedBooksRecord/InterestedBookRecordViewController.swift
@@ -6,24 +6,63 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import CoreData
 
-class InterestedBookRecordViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class InterestedBookRecordViewController: BaseViewController {
+    private let viewModel: InterestedBookRecordViewModelType
+    
+    // MARK: UI Components
+    private let deleteButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "trash"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    // MARK: init
+    init(viewModel: InterestedBookRecordViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    // MARK: SetUp ViewController
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setViewController() {
+        super.setViewController()
+        [].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        super.setConstraints()
+    }
+    
+    override func setNavigation() {
+        super.setNavigation()
+        let rightDeleteButton = UIBarButtonItem(customView: deleteButton)
+        self.navigationItem.rightBarButtonItem = rightDeleteButton
+    }
+    
+    override func bind() {
+        super.bind()
+        // Input
+        deleteButton.rx.tap
+            .bind(to: viewModel.didDeleteButtonTapped)
+            .disposed(by: disposeBag)
+    }
 }
