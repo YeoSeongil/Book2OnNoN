8770acb76902194c70b6f83b77ac28aba8a5497e
YeoSeongil <seongil_yeo@naver.com>
2024-05-14 15:32:28 +0900

    [ADD] ADD InterestedBookRecordContentView

---
+++ Book2OnNoN/Source/Views/RecordDetail/InterestedBooksRecord/Components/InterestedBookRecordContentView.swift
@@ -0,0 +1,20 @@
+//
+//  InterestedBookContentView.swift
+//  Book2OnNoN
+//
+//  Created by 여성일 on 5/14/24.
+//
+
+import UIKit
+
+class InterestedBookContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
