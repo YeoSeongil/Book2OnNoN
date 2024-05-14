a6299c544d994f26803c7a40b8f37cdaaa91fec5
YeoSeongil <seongil_yeo@naver.com>
2024-05-14 16:43:09 +0900

    [FEAT] FEAT InterestedBooksRecordViewModel NotificationObservers 추가, InterestedAssessmentUpdate구현, InterestedAssessmentUpdate에 필요한 프로토콜 프로퍼티, 변수 추가

--- Book2OnNoN/Source/Views/RecordDetail/InterestedBooksRecord/ViewModel/InterestedBooksRecordViewModel.swift
+++ Book2OnNoN/Source/Views/RecordDetail/InterestedBooksRecord/ViewModel/InterestedBooksRecordViewModel.swift
@@ -22,22 +22,45 @@ enum InterestedBookRecordEditProcedureType  {
 protocol InterestedBookRecordViewModelType {
     // Input
     var didDeleteButtonTapped: AnyObserver<Void> { get }
+    var didEditInterestedAssessmentSaveButtonTapped: AnyObserver<Void> { get }
+    var didEditInterestedAssessmentValue: AnyObserver<String> { get }
     
     // Output
     var resultInterestedBooksRecordData: Driver<[InterestedReadingBooks]> { get }
+    var resultInterestedBookRecordDeleteProcedureType: Driver<InterestedBookRecordDeleteProcedureType> { get }
+    var resultInterestedBookRecordEditProcedureType: Driver<InterestedBookRecordEditProcedureType> { get }
 }
 
 class InterestedBooksRecordViewModel {
+    private let disposeBag = DisposeBag()
     private let interestedBookRecordData: InterestedReadingBooks
     
     // Input
     private let inputDeleteButtonTapped = PublishSubject<Void>()
+    private let inputEditInterestedAssessmentSaveButtonTapped = PublishSubject<Void>()
+    private let inputEditInterestedAssessmentValue = PublishSubject<String>()
    
     // Output
     private let outputInterestedBooksRecordData = BehaviorRelay<[InterestedReadingBooks]>(value: [])
+    private let outputInterestedBookRecordDeleteProcedureType = PublishRelay<InterestedBookRecordDeleteProcedureType>()
+    private let outputInterestedBookRecordEditProcedureType = PublishRelay<InterestedBookRecordEditProcedureType>()
     
     init(interestedBookRecordData: InterestedReadingBooks) {
         self.interestedBookRecordData = interestedBookRecordData
+        tryFetchData()
+        tryInterestedAssessmentUpdate()
+        setupNotificationObservers()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.NSManagedObjectContextDidSave, object: nil)
+    }
+    
+    private func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleCoreDataChange), name: NSNotification.Name.NSManagedObjectContextDidSave, object: nil)
+    }
+    
+    @objc private func handleCoreDataChange() {
         tryFetchData()
     }
     
@@ -45,6 +68,27 @@ class InterestedBooksRecordViewModel {
         let predicate = NSPredicate(format: "isbn == %@", self.interestedBookRecordData.isbn ?? "")
         guard let books = CoreDataManager.shared.fetchData(InterestedReadingBooks.self, predicate: predicate) else { return }
         outputInterestedBooksRecordData.accept(books)
+    }
+    
+    private func tryInterestedAssessmentUpdate() {
+        inputEditInterestedAssessmentSaveButtonTapped
+            .withLatestFrom(inputEditInterestedAssessmentValue)
+            .subscribe(onNext: { comment in
+                let predicate = NSPredicate(format: "isbn == %@", self.interestedBookRecordData.isbn ?? "")
+                if let books = CoreDataManager.shared.fetchData(InterestedReadingBooks.self, predicate: predicate), let book = books.first {
+                    book.comment = comment
+                }
+                
+                CoreDataManager.shared.saveData { result in
+                    switch result {
+                    case .success:
+                        self.outputInterestedBookRecordEditProcedureType.accept(.successEdit)
+                    case .failure(let error):
+                        self.outputInterestedBookRecordEditProcedureType.accept(.failureEdit)
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
 
@@ -54,9 +98,24 @@ extension InterestedBooksRecordViewModel: InterestedBookRecordViewModelType {
         inputDeleteButtonTapped.asObserver()
     }
     
+    var didEditInterestedAssessmentSaveButtonTapped: AnyObserver<Void> {
+        inputEditInterestedAssessmentSaveButtonTapped.asObserver()
+    }
+    
+    var didEditInterestedAssessmentValue: AnyObserver<String> {
+        inputEditInterestedAssessmentValue.asObserver()
+    }
+    
     // Output
     var resultInterestedBooksRecordData: Driver<[InterestedReadingBooks]> {
         outputInterestedBooksRecordData.asDriver(onErrorDriveWith: .empty())
     }
     
+    var resultInterestedBookRecordDeleteProcedureType: Driver<InterestedBookRecordDeleteProcedureType> {
+        outputInterestedBookRecordDeleteProcedureType.asDriver(onErrorDriveWith: .empty())
+    }
+    
+    var resultInterestedBookRecordEditProcedureType: Driver<InterestedBookRecordEditProcedureType> {
+        outputInterestedBookRecordEditProcedureType.asDriver(onErrorDriveWith: .empty())
+    }
 }
