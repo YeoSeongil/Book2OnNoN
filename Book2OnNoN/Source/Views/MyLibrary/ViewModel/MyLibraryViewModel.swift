9b7209062324fc9646e2f3d6821727ca1175dce6
YeoSeongil <seongil_yeo@naver.com>
2024-05-16 13:41:17 +0900

    [FEAT] FEAT MyLibraryViewModel fetchData 구현, NotificationObservers Setup

--- Book2OnNoN/Source/Views/MyLibrary/ViewModel/MyLibraryViewModel.swift
+++ Book2OnNoN/Source/Views/MyLibrary/ViewModel/MyLibraryViewModel.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+protocol MyLibraryViewModelType {
+    // Output
+    var finishedReadingBookRecordItem: Driver<[FinishedReadingBooks]> { get }
+}
+
+class MyLibraryViewModel {
+     private let disposeBag = DisposeBag()
+    
+    // Output
+    private let outputFinishedReadingBookRecordItem = BehaviorRelay<[FinishedReadingBooks]>(value: [])
+    
+    init()  {
+        fetchData()
+        setupNotificationObservers()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.NSManagedObjectContextDidSave, object: nil)
+    }
+
+    private func fetchData() {
+        guard let finishedReadingBookItem = CoreDataManager.shared.fetchData(FinishedReadingBooks.self) else { return }
+        outputFinishedReadingBookRecordItem.accept(finishedReadingBookItem)
+    }
+    
+    private func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleCoreDataChange), name: NSNotification.Name.NSManagedObjectContextDidSave, object: nil)
+    }
+    
+    @objc private func handleCoreDataChange() {
+        fetchData()
+    }
+}
+
+extension MyLibraryViewModel: MyLibraryViewModelType {
+    // Output
+    var finishedReadingBookRecordItem: Driver<[FinishedReadingBooks]> {
+        outputFinishedReadingBookRecordItem.asDriver(onErrorDriveWith: .empty())
+    }
+}
