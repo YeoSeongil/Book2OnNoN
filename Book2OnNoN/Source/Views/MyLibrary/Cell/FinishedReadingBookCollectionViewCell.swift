6a73030f5b7a098445fd54c3c7d37a0296cec577
YeoSeongil <seongil_yeo@naver.com>
2024-05-16 12:51:53 +0900

    [ADD] ADD FinishedReadingBookCollectionViewCell

---
+++ Book2OnNoN/Source/Views/MyLibrary/Cell/FinishedReadingBookCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FinishedReadingBookCollectionViewCell.swift
+//  Book2OnNoN
+//
+//  Created by 여성일 on 5/16/24.
+//
+
+import UIKit
+
+class FinishedReadingBookCollectionViewCell: UICollectionViewCell {
+    
+}
