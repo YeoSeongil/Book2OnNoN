c995acaef33ef4c5091291dbcd189801b653f2df
YeoSeongil <seongil_yeo@naver.com>
2024-05-16 13:38:05 +0900

    [FEAT] FEAT FinishedReadingBookCollectionViewCell UI Components구현, 데이터 바인딩 구현

--- Book2OnNoN/Source/Views/MyLibrary/Cell/FinishedReadingBookCollectionViewCell.swift
+++ Book2OnNoN/Source/Views/MyLibrary/Cell/FinishedReadingBookCollectionViewCell.swift
@@ -6,7 +6,54 @@
 //
 
 import UIKit
+import SnapKit
 
 class FinishedReadingBookCollectionViewCell: UICollectionViewCell {
+    static let id: String = "FinishedReadingBookCollectionViewCell"
     
+    private let finishedReadingBookThumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.backgroundColor = .clear
+        return imageView
+    }()
+    
+    // MARK: init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setCell()
+        setAddViews()
+        setConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: SetUp Cell
+    private func setCell() {
+        backgroundColor = .black
+    }
+    
+    private func setAddViews() {
+        [finishedReadingBookThumbnailImageView].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func setConstraint() {
+        finishedReadingBookThumbnailImageView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.verticalEdges.equalToSuperview()
+        }
+    }
+
+    // MARK: Configuration
+    func configuration(book: FinishedReadingBooks) {
+        if let imageURL = book.thumbnail {
+            finishedReadingBookThumbnailImageView.setImageKingfisher(with: imageURL)
+        } else {
+            finishedReadingBookThumbnailImageView.image = UIImage(named: "unknownBook")
+        }
+    }
 }
