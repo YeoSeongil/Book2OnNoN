573e74848dd8626c68ffe9775fc540e7d2b87082
YeoSeongil <seongil_yeo@naver.com>
2024-05-16 15:52:02 +0900

    [ADD] ADD EditBookAssessmentViewController

---
+++ Book2OnNoN/Source/Views/RecordDetail/FinishedReadingBookRecord/Modal/EditBookAssessmentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditBookAssessmentViewController.swift
+//  Book2OnNoN
+//
+//  Created by 여성일 on 5/16/24.
+//
+
+import UIKit
+
+class EditBookAssessmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
